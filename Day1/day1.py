4b34f4867a2ebd4e9a1b9e234867de821800d151
Shubham Prasar <prasarshubham04@gmail.com>
2025-08-03 19:35:45 +0530

    Python Code

---
+++ Day1/day1.py
@@ -0,0 +1,6 @@
+print("Hello world!")
+
+print("Welcome to the Band Name Generator.")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of a pet?\n")
+print("Your band name could be " + city + " " + pet)
